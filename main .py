5da96e283923f38982cb718eb77c440f8ccd382c
M. Ali Erdoğan <muhammetalie19@gmail.com>
2023-03-26 12:34:39 +0300

    Add files via upload

---
+++ main .py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    audio = r.listen(source)
+    voice = r.recognize_google(audio, language='tr-TR')
+
+    print(voice)
