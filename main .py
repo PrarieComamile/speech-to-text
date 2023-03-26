a6748ba7c10efa751e4c6c8f14696fa283c6f1bf
M. Ali Erdoğan <muhammetalie19@gmail.com>
2023-03-26 12:50:47 +0300

    Update main .py

--- main .py
+++ main .py
@@ -5,5 +5,8 @@ r = sr.Recognizer()
 with sr.Microphone() as source:
     audio = r.listen(source)
     voice = r.recognize_google(audio)
+    
+    text = open("text.txt", "w")
+    text.write(voice)
 
     print(voice)
