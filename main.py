95d211cd53ba8b5a808f4df928a074905ad90e98
M. Ali Erdoğan <muhammetalie19@gmail.com>
2023-03-30 20:51:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import speech_recognition as sr
 
+r = sr.Recognizer()
+
+while True:
+    
+    try:
+        with sr.Microphone() as source:
+            print("Speak...")
+            audio = r.listen(source)
+            voice = r.recognize_google(audio, language="tr-TR")
+            
+            text = open("text.txt", "w")
+            text.write(voice+"\n")
+
+            print(voice)
+        
+    except:
+        raise ValueError("Time Out!")
